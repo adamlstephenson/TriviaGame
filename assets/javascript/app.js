b75c5e295fc9b54dc87b967bd92384db7861e684
Adam Stephenson <adamstephenson@Adams-MacBook-Pro.local>
2018-03-13 20:19:33 -0500

    more styling and hide method added to start button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,12 @@
 // Global variables
 //=============================================================
-
 var timeRemaining;
 var questionArray = [];
 var answerArray = [];
 var correctAnswers;
 var incorrectAnswers;
 var unanswered;
+
+$("#start").on("click", function() {
+    $("#start").hide();
+});
