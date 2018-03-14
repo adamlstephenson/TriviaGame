df6b6a066d0ff9b0c858d0864f57b9af9e25c868
Adam Stephenson <adamstephenson@Adams-MacBook-Pro.local>
2018-03-13 19:53:07 -0500

    bootstrap, styling, and basic variable setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+// Global variables
+//=============================================================
+
+var timeRemaining;
+var questionArray = [];
+var answerArray = [];
+var correctAnswers;
+var incorrectAnswers;
+var unanswered;
