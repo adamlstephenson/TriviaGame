2d1a6d11648d1c926e46681109b95fa4bf29a650
Adam Stephenson <adamstephenson@Adams-MacBook-Pro.local>
2018-03-18 22:54:32 -0500

    on the right track to user input

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,52 +17,52 @@ var correct = 0;
 var questionList = [{
     question: "What is the only species of trout native to Minnesota?",
     answers: ["Brook Trout", "Rainbow Trout", "Brown Trout", "Cutthroat Trout"],
-    correctAnswer: 0
+    correctAnswer: "A"
     },
 {
     question: "Which species is a hybrid cross between a female brown trout and a male brook trout",
     answers: ["Gila Trout", "Golden Trout", "Tiger Trout", "Bull Trout"],
-    correctAnswer: 2
+    correctAnswer: "C"
     },
 {
     question: "When do brown trout spawn?",
     answers: ["Spring", "Summer", "Fall", "Winter"],
-    correctAnswer: 2
+    correctAnswer: "C"
     },
 {
     question: "A trout's age can be determined by what feature?",
     answers: ["Colorization", "Scales", "Length", "Adipose fin"],
-    correctAnswer: 1
+    correctAnswer: "B"
 },
 {
     question: "Trout lay their eggs in nests called what?",
     answers: ["Cribs", "Beds","Incubators","Redds"],
-    correctAnswer: 3
+    correctAnswer: "D"
 },
 {
     question: "Brown trout can be found in which country?",
     answers: ["Sri Lanka", "Argentina", "Zimbabwe", "All of the above"],
-    correctAnswer: 3
+    correctAnswer: "D"
 },
 {
     question: "What is the largest species of trout in the world?",
     answers: ["Bull Trout", "Taimen", "Steelhead", "Tiger Trout"],
-    correctAnswer: 1
+    correctAnswer: "B"
     },
 {
     question: "How large was the rod and reel record lake trout?",
     answers: ["72 pounds", "33 pounds", "96 pounds", "21 pounds"],
-    correctAnswer: 0
+    correctAnswer: "Z"
     },
 {
     question: "What is the scientific name for rainbow trout?",
     answers: ["Salvelinus fontinalis", "Oncorhynchus mykiss", "Salmo trutta", "Cyprinus carpio"],
-    correctAnswer: 1
+    correctAnswer: "B"
 },
 {
     question: "A trout's swim bladder helps it do what?",
     answers: ["Urinate", "Hold air as an oxygen reserve", "Float", "Prevent infections"],
-    correctAnswer: 2
+    correctAnswer: "C"
     },
 
 ];
@@ -86,18 +86,35 @@ function showQuestion () {
     question = questionList[0].question.toString();
     console.log(question);
     answer = questionList[0].answers;
+    rightAnswer = questionList[0].correctAnswer;
         choiceA = answer[0];
         choiceB = answer[1];
         choiceC = answer[2];
         choiceD = answer[3];
     $("#questions").html("<h3>" + question + "</h3>")
-    $("#q1").html("<input type='radio' name = 'choices value='A'> " + choiceA + "<br>");
-    $("#q2").html("<input type='radio' name = 'choices value='A'> " + choiceB + "<br>");
-    $("#q3").html("<input type='radio' name = 'choices value='A'> " + choiceC + "<br>");
-    $("#q4").html("<input type='radio' name = 'choices value='A'> " + choiceD + "<br>");
+    $("#q1").html("<input type='radio' name = 'choices' value='A'> " + choiceA + "<br>");
+    $("#q2").html("<input type='radio' name = 'choices' value='A'> " + choiceB + "<br>");
+    $("#q3").html("<input type='radio' name = 'choices' value='A'> " + choiceC + "<br>");
+    $("#q4").html("<input type='radio' name = 'choices' value='A'> " + choiceD + "<br>");
     $("#submit").html("<input type='submit'>");
 
 }
+}
+
+function checkAnswer() {
+    $("#submit").on("click", function() {
+    choices = document.getElementsByName("choices");
+        for(j = 0; j < choices.length; j++) {
+            if (choices[i].checked) {
+                choice = choices[i].value;
+            }
+        }
+        console.log(choice)
+        if (choice === rightAnswer) {
+            correct++;
+        }
+
+    })
 }
 
 
@@ -113,6 +130,7 @@ $("#start").on("click", function() {
     $("#start").hide();
    // $("#timeremaining").text("Time Remaining: " + questionTimer + " seconds");
     showQuestion();
+    checkAnswer();
         })
 
 
